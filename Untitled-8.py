a8454cc4607a440011b3139cad9370b5bcc93939
VineetJohn88156 <vineetjohn88156@gmail.com>
2024-10-27 15:46:40 +0530

    Delete Untitled-8.py

--- Untitled-8.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(s):
-    return s == s[::-1]
-
-# Example
-string = "madam"
-if is_palindrome(string):
-    print(f"{string} is a palindrome.")
-else:
-    print(f"{string} is not a palindrome.")
