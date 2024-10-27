f4fd0b5c1589050491fff90eb1c9b5c153f5931a
VineetJohn88156 <vineetjohn88156@gmail.com>
2024-10-27 15:45:05 +0530

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-def string_operations():
-    str1 = "GFG"
-    str2 = "AI-ml group"
-    concatenated = str1 + " " + str2
-    print("Concatenated String:", concatenated)
-    
- 
-string_operations()
