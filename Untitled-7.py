8e914ac0cdcf2a40edd9c4a52c4a6c43ee0fbdc7
VineetJohn88156 <vineetjohn88156@gmail.com>
2024-10-27 15:46:31 +0530

    Delete Untitled-7.py

--- Untitled-7.py
+++
@@ -1,8 +0,0 @@
-def compound_interest(principal, rate, time):
-    amount = principal * (1 + rate / 100) ** time
-    interest = amount - principal
-    print(f"Compound Interest: {interest}")
-    print(f"Total Amount: {amount}")
-
-# Example
-compound_interest(1000, 5, 2)
